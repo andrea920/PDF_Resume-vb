17962f688ba361aac5139b1ab297ea245a929821
Ma. Andrea G. Perido <andreamaperido@gmail.com>
2022-07-28 07:09:07 +0800

    tried to make a variable to put a style and it worked

--- Form1.vb
+++ Form1.vb
@@ -6,23 +6,34 @@ Public Class Form1
     Dim GetJsonFile As String = File.ReadAllText("mydetails.json")
     Dim JsonOutput As MyDetails = JsonConvert.DeserializeObject(Of MyDetails)(GetJsonFile)
 
+
     Private Sub BtnCreatePDF_Click(sender As Object, e As EventArgs) Handles BtnCreatePDF.Click
         Dim CreatedPDF As Document = New Document
         PdfWriter.GetInstance(CreatedPDF, New FileStream("Perido, Ma. Andrea.pdf", FileMode.Create))
-
         CreatedPDF.Open()
-        CreatedPDF.Add(New Paragraph(JsonOutput.Name))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Objectives))
-        CreatedPDF.Add(New Paragraph(JsonOutput.ContactNumber))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Address))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Age))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Email))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Education))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Skills))
-        CreatedPDF.Add(New Paragraph(JsonOutput.Name2))
+        Dim Name As Paragraph = New Paragraph(JsonOutput.Name)
+
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Name))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Objectives))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.ContactNumber))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Address))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Age))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Email))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Education))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Skills))
+        'CreatedPDF.Add(New Paragraph(JsonOutput.Name2))
+
+
+        Name.Font.Size = 30
+        CreatedPDF.Add(Name)
+
+
+
+
+
+
         CreatedPDF.Close()
         MsgBox("Resume Created (PDF)")
-
     End Sub
 
     Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
