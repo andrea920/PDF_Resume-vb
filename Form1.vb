2a1d5021e35dbedafc4ee9485a1a2297845540dd
Ma. Andrea G. Perido <andreamaperido@gmail.com>
2022-07-28 08:28:37 +0800

    done with alignment and fontsize, i add picture in netwindows folder and edit my json

--- Form1.vb
+++ Form1.vb
@@ -13,15 +13,26 @@ Public Class Form1
         PdfWriter.GetInstance(CreatedPDF, New FileStream("Perido, Ma. Andrea.pdf", FileMode.Create))
         CreatedPDF.Open()
         Dim Name As Paragraph = New Paragraph(JsonOutput.Name & vbLf & vbLf)
-        Dim MyObjectives As Paragraph = New Paragraph("OBJECTIVES" & vbLf & vbLf)
-        Dim Divider As LineSeparator = New LineSeparator(5.0F, 110.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
-        Dim Objectives As Paragraph = New Paragraph(JsonOutput.Objectives)
+
+        Dim Obj As Paragraph = New Paragraph("OBJECTIVES" & vbLf & vbLf)
+        Dim Divider1 As LineSeparator = New LineSeparator(5.0F, 110.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
+        Dim Objectives As Paragraph = New Paragraph(JsonOutput.Objectives & vbLf & vbLf)
+
+        Dim Data As Paragraph = New Paragraph("DATA" & vbLf & vbLf)
+        Dim Divider2 As LineSeparator = New LineSeparator(5.0F, 110.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
         Dim ContactNumber As Paragraph = New Paragraph(JsonOutput.ContactNumber)
         Dim Address As Paragraph = New Paragraph(JsonOutput.Address)
         Dim Age As Paragraph = New Paragraph(JsonOutput.Age)
-        Dim Email As Paragraph = New Paragraph(JsonOutput.Email)
-        Dim Education As Paragraph = New Paragraph(JsonOutput.Education)
-        Dim Skills As Paragraph = New Paragraph(JsonOutput.Skills)
+        Dim Email As Paragraph = New Paragraph(JsonOutput.Email & vbLf & vbLf)
+
+        Dim Educ As Paragraph = New Paragraph("EDUCATION" & vbLf & vbLf)
+        Dim Divider3 As LineSeparator = New LineSeparator(5.0F, 110.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
+        Dim Education As Paragraph = New Paragraph(JsonOutput.Education & vbLf & vbLf)
+
+        Dim Skl As Paragraph = New Paragraph("SKILLS" & vbLf & vbLf)
+        Dim Divider4 As LineSeparator = New LineSeparator(5.0F, 110.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
+        Dim Skills As Paragraph = New Paragraph(JsonOutput.Skills & vbLf & vbLf)
+
         Dim Name2 As Paragraph = New Paragraph(JsonOutput.Name2)
 
 
@@ -30,15 +41,61 @@ Public Class Form1
         Name.Alignment = Element.ALIGN_CENTER
         CreatedPDF.Add(Name)
 
-        MyObjectives.Font.Size = 20
-        MyObjectives.Alignment = Element.ALIGN_LEFT
-        CreatedPDF.Add(MyObjectives)
+        Obj.Font.Size = 20
+        Obj.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Obj)
 
-        CreatedPDF.Add(Divider)
+        CreatedPDF.Add(Divider1)
 
         Objectives.Font.Size = 12
         Objectives.Alignment = Element.ALIGN_LEFT
         CreatedPDF.Add(Objectives)
+
+        Data.Font.Size = 20
+        Data.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Data)
+
+        CreatedPDF.Add(Divider2)
+
+        ContactNumber.Font.Size = 12
+        ContactNumber.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(ContactNumber)
+
+        Address.Font.Size = 12
+        Address.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Address)
+
+        Age.Font.Size = 12
+        Age.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Age)
+
+        Email.Font.Size = 12
+        Email.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Email)
+
+        Educ.Font.Size = 20
+        Educ.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Educ)
+
+        CreatedPDF.Add(Divider3)
+
+        Education.Font.Size = 12
+        Education.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Education)
+
+        Skl.Font.Size = 20
+        Skl.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Skl)
+
+        CreatedPDF.Add(Divider4)
+
+        Skills.Font.Size = 12
+        Skills.Alignment = Element.ALIGN_LEFT
+        CreatedPDF.Add(Skills)
+
+        Name2.Font.Size = 18
+        Name2.Alignment = Element.ALIGN_RIGHT
+        CreatedPDF.Add(Name2)
 
         CreatedPDF.Close()
         MsgBox("Resume Created (PDF)")
